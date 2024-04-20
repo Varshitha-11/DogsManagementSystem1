73e956c655426b40d6002af573596a0611745f90
Varshitha-11 <varshithagunnala.kc@gmail.com>
2024-04-19 23:10:47 -0500

    Added my project

---
+++ src/main/java/com/gunnalaClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,10 @@
+package com.gunnalaClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.gunnalaClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+	
+
+}
